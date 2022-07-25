59b25c65ffb3cce7440c004061d4a67a221b7df6
illuzX <98640218+illuzX@users.noreply.github.com>
2022-07-25 14:48:03 +0530

    update Script.py

--- Script.py
+++ Script.py
@@ -3,8 +3,7 @@ class script(object):
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂"""
-    ABOUT_TXT = """
-•>C𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/grayhathacker676>IlluzX</a>
+    ABOUT_TXT = """ •>C𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/grayhathacker676>IlluzX</a>
 •>L𝙸B𝚁𝙰𝚁Y: 𝙿Y𝚁𝙾G𝚁A𝙼
 •>L𝙰N𝙶𝚄A𝙶E: 𝙿Y𝚃𝙷Ø𝙽 𝟹
 •>D𝙰𝚃A B𝙰S𝙴: M𝙾𝙽G𝙾 D𝙱
