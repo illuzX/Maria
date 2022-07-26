46ea7a72522b7ecefc61a98754e45e27e488d91a
illuzX <98640218+illuzX@users.noreply.github.com>
2022-07-27 02:55:02 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -97,12 +97,14 @@ async def start(client, message):
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
+        kj=await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
+        await asyncio.sleep(60)
+        await kj.delete()
         return
     data = message.command[1]
     try:
