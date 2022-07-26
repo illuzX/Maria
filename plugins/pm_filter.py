8cddc3f4e6e27a7baa4168aea0adab180bf4d025
illuzX <98640218+illuzX@users.noreply.github.com>
2022-07-27 01:08:21 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -439,14 +439,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode='html'
         )
     elif query.data == "ddl":
-        reply_markup = InlineKeyboardMarkup
-        await query.message.edit_text(
-            text=script.DIMR
-            reply_markup=reply_markup,
-            parse_mode='html'
-      )
+       buttons = [[
             InlineKeyboardButton('🏠 Home', callback_data='start'),
             InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.DIMR.format(query.from_user.mention),
+            reply_markup=reply_markup,
+            parse_mode='html'
+       )
+            
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('🤖 Updates', url='https://t.me/uzx_bots'),
