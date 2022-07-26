7c4a746191f505a236ee8d40629048b7730228f5
illuzX <98640218+illuzX@users.noreply.github.com>
2022-07-26 22:52:24 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -708,7 +708,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}\ntotal Files {math.ceil(int(files) / 10)} "
+        cap = f"Here is what i found for your query {search}\ntotal Files {math.ceil(int(file_files) / 10000)} "
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
