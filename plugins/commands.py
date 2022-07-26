115f486bee907faa9370a7de0300314318a85fd1
illuzX <98640218+illuzX@users.noreply.github.com>
2022-07-27 01:27:30 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,17 +43,17 @@ async def start(client, message):
     if len(message.command) != 2:
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-            ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/uzx_bots')
-            ],[
+        ], [
+            InlineKeyboardButton('OTT UPDATES🗃️',url='https://t.me/MalayalamOTTUpdatesMvb')
+        ], [
+            InlineKeyboardButton('❗ Disclaimer ❗', callback_data='ddl')
+        ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await query.message.edit_text(
+            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
