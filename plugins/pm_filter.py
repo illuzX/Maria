66ae4bd121009547bb27c817c5286545cfef5b29
illuzX <98640218+illuzX@users.noreply.github.com>
2022-07-27 01:25:52 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -406,13 +406,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/uzx_bots')
+            InlineKeyboardButton('OTT UPDATES🗃️',url='https://t.me/MalayalamOTTUpdatesMvb')
+        ], [
+            InlineKeyboardButton('❗ Disclaimer ❗', callback_data='ddl')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
-      ], [
-            InlineKeyboardButton('❗Disclaimer' , callback_data='ddl')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
