29f3156b54c9034c6463d034f3ef6d37e8dbb87a
illuzX <98640218+illuzX@users.noreply.github.com>
2022-07-27 02:12:32 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,11 +51,9 @@ async def start(client, message):
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode='html'
-        )
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
