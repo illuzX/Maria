1faf7d8d1851a357aff4b52fb3ad51169569849b
illuzX <98640218+illuzX@users.noreply.github.com>
2022-08-04 16:00:04 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-@Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
+@Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         f1 = await query.message.reply_text("Hy..")
