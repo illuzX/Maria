243efdc1e965fb6f537ddeb682a1569eb96117fa
illuzX <98640218+illuzX@users.noreply.github.com>
2022-08-04 18:00:54 +0530

    update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,8 @@ from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 
+uvloop.install()
+
 class Bot(Client):
 
     def __init__(self):
