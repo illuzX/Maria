93dc7c869fc4806bb3ca35929fab66f323cf1d43
illuzX <98640218+illuzX@users.noreply.github.com>
2022-08-09 11:37:48 +0530

    update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -20,14 +20,6 @@ async def allowed(_, __, message):
         return True
     return False
 
-class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    file_type = fields.StrField(allow_none=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
 
 @Client.on_message(filters.command(['link', 'plink']) & filters.create(allowed))
 async def gen_link_s(bot, message):
