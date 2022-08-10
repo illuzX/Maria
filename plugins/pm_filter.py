c777aecc5ce055775ea10967e50e41ab9d89a5dc
illuzX <98640218+illuzX@users.noreply.github.com>
2022-08-10 15:31:31 +0530

    Now Anyone Can use Next /Pre Buttons

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -40,8 +40,6 @@ async def give_filter(client, message):
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
-    if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("Ayo Ayo! ask own request", show_alert=True)
     try:
         offset = int(offset)
     except:
