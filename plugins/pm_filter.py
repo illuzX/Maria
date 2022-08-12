8c85eb67548a0ee6ce8db5dec0dbd57af78d1b95
illuzX <98640218+illuzX@users.noreply.github.com>
2022-08-13 00:35:48 +0530

    Extra Future button error ❌

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -533,7 +533,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.message.edit_text(
             text=script.EXTRAMOD_TXT,
             reply_markup=reply_markup,
-            parse_mode='html'
+            parse_mode=enums.ParseMode.HTML
         )
     elif query.data == "admin":
         buttons = [[
