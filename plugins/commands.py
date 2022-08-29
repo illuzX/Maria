6f7180f0b7c0980e70cc3f84f9e9111d65f6f2a3
illuzX <98640218+illuzX@users.noreply.github.com>
2022-08-29 19:49:48 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,7 +21,7 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.from_user.id not in ADMINS:
-        await m.delete()
+        await message.delete()
         return
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
