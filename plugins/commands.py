b3d48bbb56e11fb44ae37842ea402a936d31d267
illuzX <98640218+illuzX@users.noreply.github.com>
2022-09-28 02:39:44 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,8 +51,8 @@ async def start(client, message):
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        kj=await message.reply_photo(
-            photo=random.choice(PICS),photo=random.choice(PICS),
+       kj=await message.reply_photo(
+            photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
