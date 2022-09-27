6995a47c7f4dc71a0db6799cefdeae7d7364ceca
illuzX <98640218+illuzX@users.noreply.github.com>
2022-09-28 02:47:15 +0530

    Syndex

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,7 +98,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         kj=await message.reply_photo(
-            photo=random.choice(PICS),photo=random.choice(PICS),
+            photo=random.choice(PICS),
             caption=script.START_TXT.formatscript.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
