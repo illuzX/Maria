c8f0b360c3fe05db5bf10607a7caa1dac450747f
illuzX <98640218+illuzX@users.noreply.github.com>
2022-09-28 03:27:24 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -413,7 +413,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        kj=await query.message.edit_photo(
+        kj=await query.message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
