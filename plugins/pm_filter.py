4c5ec55cae762353d067f35d3207cd8783f4d38c
illuzX <98640218+illuzX@users.noreply.github.com>
2022-10-06 22:39:57 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -844,5 +844,6 @@ async def manual_filters(client, message, text=False):
                       #  await rm.delete()
                 except Exception as e:
                     logger.exception(e)
+                break
     else:
-        await query.answer("Okda", show_alert=True)
+        return False
