6fedd84ca59566d0df73ab3c47fd0aad9438f221
illuzX <98640218+illuzX@users.noreply.github.com>
2022-10-06 19:03:52 +0530

    update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -630,9 +630,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
     await query.answer('Prosessing...Request')
-    else:
-        await query.answer("Please Request",show_alert=True)
-
 
 async def auto_filter(client, msg, spoll=False):
     if not spoll:
