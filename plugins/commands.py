3bdb473e9800b91eec9da99373f6a3e05cb37da7
illuzX <98640218+illuzX@users.noreply.github.com>
2022-10-17 22:29:51 +0530

    update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,10 +43,10 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
+        [
             InlineKeyboardButton('⚙️Help Module🔧 ', callback_data='help'),
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
+        ]
+        reply_markup = InlineKeyboardMarkup
         kj=await message.reply_photo(
             photo=random.choice(STAMP),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -85,10 +85,10 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [[
+        [
             InlineKeyboardButton('⚙️Help Module🔧 ', callback_data='help'),
-        ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
+        ]
+        reply_markup = InlineKeyboardMarkup
         kj=await message.reply_photo(
             photo=random.choice(STAMP),
             caption=script.START_TXT.formatscript.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
