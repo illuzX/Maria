3c8ff2d0eb30bc932feb3932794e6c1341ad7ffe
illuzX <98640218+illuzX@users.noreply.github.com>
2023-01-10 11:31:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -844,6 +844,17 @@ async def advantage_spell_chok(client, msg):
         caption=(script.ImDb_msg.format(reqstr.mention)),
         reply_markup=InlineKeyboardMarkup(btn)
         )
+    try:
+        if settings['auto_delete']:
+            await asyncio.sleep(600)
+            await spell_check_del.delete()
+    except KeyError:
+            grpid = await active_connection(str(message.from_user.id))
+            await save_group_settings(grpid, 'auto_delete', True)
+            settings = await get_settings(message.chat.id)
+            if settings['auto_delete']:
+                await asyncio.sleep(600)
+                await spell_check_del.delete()
        
 
 async def manual_filters(client, message, text=False):
