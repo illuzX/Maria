769c98b7c0d4de1cb29c14f3264f393eff61282f
illuzX <98640218+illuzX@users.noreply.github.com>
2023-01-17 18:29:31 +0530

    revert To Up

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-echo "EOL: Exiting..."
+
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/illuzX/Maria.git /Maria
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Maria
+fi
+cd /Maria
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
